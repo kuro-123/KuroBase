63d7cc4a702547a1747e34d1aea62cb5dc0046a5
kuro-123 <noripiro@sage.ocn.ne.jp>
2020-05-06 13:25:12 +0900

    fix ri command

--- src/main/java/host/kuro/kurobase/commands/RiCommand.java
+++ src/main/java/host/kuro/kurobase/commands/RiCommand.java
@@ -4,10 +4,12 @@ import host.kuro.kurobase.KuroBase;
 import host.kuro.kurobase.lang.Language;
 import host.kuro.kurobase.utils.SoundUtils;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class RiCommand implements CommandExecutor {
 
@@ -31,7 +33,16 @@ public class RiCommand implements CommandExecutor {
             SoundUtils.PlaySound(player,"cancel5", false);
             return false;
         }
+
+        for(ItemStack item : player.getInventory().getContents())
+        {
+            if (item == null) continue; // null check
+            player.getInventory().remove(item);
+        }
         player.updateInventory();
+
+        player.sendMessage(ChatColor.DARK_GREEN + Language.translate("commands.ri.success"));
+        SoundUtils.PlaySound(player,"switch1", false);
         return true;
     }
 }
