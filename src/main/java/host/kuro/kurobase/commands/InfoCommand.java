8fc885366169ae5bb5652873469a00d07e7479e1
kuro-123 <noripiro@sage.ocn.ne.jp>
2020-05-10 13:03:43 +0900

    info command add money info

--- src/main/java/host/kuro/kurobase/commands/InfoCommand.java
+++ src/main/java/host/kuro/kurobase/commands/InfoCommand.java
@@ -2,7 +2,9 @@ package host.kuro.kurobase.commands;
 
 import host.kuro.kurobase.KuroBase;
 import host.kuro.kurobase.lang.Language;
+import host.kuro.kurobase.utils.PlayerUtils;
 import host.kuro.kurobase.utils.SoundUtils;
+import host.kuro.kurobase.utils.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -25,6 +27,12 @@ public class InfoCommand implements CommandExecutor {
             return false;
         }
         final Player player = (Player)sender;
+
+        // money info
+        String money = StringUtils.numFmt.format(PlayerUtils.GetMoney(KuroBase.getDB(), player));
+        String money_message = String.format(Language.translate("plugin.money.info")+": %s p", money);
+        player.sendMessage(ChatColor.DARK_GREEN + money_message);
+        // web info
         player.sendMessage(ChatColor.DARK_GREEN + plugin.getConfig().getString("Url.info"));
         player.sendMessage(ChatColor.DARK_GREEN + Language.translate("commands.info.message"));
         SoundUtils.PlaySound(player,"switch1", false);
